a25b8057249ba43f0797b0b316fd3f82e638a496
Manish Khedekar <mankhedekar96@gmail.com>
2019-02-20 15:06:11 +0530

    Theming done

--- js/script.js
+++ js/script.js
@@ -1,48 +1,85 @@
 let id = 1;
+let inputBox = document.getElementById("userBox");
+let chatBox = document.getElementById("chatBox");
+let toggleViewBtn = document.getElementById("toggleViewBtn");
+let chatContainer = document.getElementById("chatContainer");
+let chatBoxHeader = document.getElementById("chatBoxHeader");
+
+
 
 // When user hit send the message following function executes. 
 const userAction = async () => {
-  let input = document.getElementById("userBox").value;
-  document.getElementById("userBox").value = "";
-  addMe(input, "sent", id++);
-  const response = await fetch("http://localhost:8090", { // Your DialogFlow gateway api url
-    method: "POST",
-    body: JSON.stringify({
-      q: input,
-      session_id: "test",
-      lang: "hi"
-    }),
-    headers: {
-      "Content-Type": "application/json"
+  let input = inputBox.value;
+  inputBox.value = "";
+  if(input!=""){
+    addMe(input, "sent", id++);
+    const response = await fetch("http://localhost:8090", { // Your DialogFlow gateway api url
+        method: "POST",
+        body: JSON.stringify({
+        q: input,
+        session_id: "test",
+        lang: "hi"
+        }),
+        headers: {
+        "Content-Type": "application/json"
+        }
+    });
+    const myJson = await response.json();
+    // do something with myJson
+    let res = myJson.queryResult.fulfillmentText;
+    addMe(res, "received", id++);
     }
-  });
-  const myJson = await response.json();
-  // do something with myJson
-  let res = myJson.queryResult.fulfillmentText;
-  addMe(res, "received", id++);
 };
 
 // This function adds messafe when message is sent or received
 function addMe(input, msgDetails, id) {
-  let chatContainer = document.getElementById("chatLog");
   chatContainer.innerHTML +=
-    '<div id="msg' + id + '" class="' + msgDetails + '">' + input + "</div>";
+    '<div id="msg' + id + '" class="' + msgDetails + ' box-shadow">' + input + "</div>";
   chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
 // Toggling chatbox (Minimizing and maximizing)
 function toggleView() {
-  let inputBox = document.getElementById("userBox");
-  let chatBox = document.getElementById("chatBox");
-  let toggleView = document.getElementById("toggleView");
-
-  if (toggleView.innerText == "-") {
-    toggleView.innerText = "^";
+  if (toggleViewBtn.innerText == "-") {
+    toggleViewBtn.innerText = "^";
     chatBox.style.height = "50px";
     inputBox.style.display = "none";
   } else {
-    toggleView.innerText = "-";
+    toggleViewBtn.innerText = "-";
     chatBox.style.height = "450px";
     inputBox.style.display = "block";
   }
 }
+
+// Theme changing function 
+function changeTheme(color){
+
+    chatBoxHeader.style.backgroundColor = color;
+    chatBoxHeader.style.color = 'white';
+    toggleViewBtn.style.backgroundColor = color;
+    toggleViewBtn.style.color = 'white';
+    chatBox.style.backgroundImage= 'linear-gradient(to bottom, white 40%, '+color+')';
+
+    let array = document.getElementsByClassName('sent');
+
+    if(array.length>0){ 
+        array = [...array,...document.getElementsByClassName('received')];
+        for(let i of array){
+            i.style.border = '1px solid '+color;
+        }
+    }
+}
+
+// Render themes
+function renderThemes(){
+    var themeContainer = document.getElementById("themes");
+    let colors = ['red','orange','darkorange','blue','skyblue','black','darkgrey','gold','tomato'];
+
+    for(let i of colors){
+        themeContainer.innerHTML += `<div class="theme-box box-shadow" style="background-color:${i};color:white;" onclick="changeTheme('${i}')">${i.toUpperCase()}</div>`;
+    }
+
+    changeTheme('orange');
+}
+
+renderThemes();
